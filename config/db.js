d9fcb1cb7c14d4e24a346021650023a00624e7cf
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2024-11-22 10:42:28 +0300

    updated secure user authentication

--- config/db.js
+++ config/db.js
@@ -11,4 +11,4 @@ const pool = mysql.createPool(
 
     }
 );
-module.exports = pool.promise()
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 //import the packages
-const express = require(express);
+const express = require('express');
 const { configDotenv } = require('dotenv');
 const session = require('express-session');
-const MYSQLStore = require('connect-mysql2')(session);
+//@ts-ignore
+const MYSQLStore = require('express-mysql-session')(session);
 const path = require('path');
-require('dotenv').config()
+require('dotenv').config();
 
-const db =require('./config/db')
-const authRoutes = require('./routes/auth') //import auth routes
+const db = require('./config/db');
+
+
+const authRoutes = require('./routes/auth'); //import auth routes
 
 //initialize server
 
@@ -16,27 +19,40 @@ const app = express();
 // set up middleware 
 app.use(express.json()); //middleware to parse JSON request body
 
+//configure session store
+const sessionStore = new MYSQLStore({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
 //set up session
 app.use(
     session({
         key: 'user_sid',
-        secret: 'process.env.SESSION_SECRET', //Secret key for signing session cookies
+        secret: process.env.SESSION_SECRET, // Secret key for signing session cookies
         resave: false,
         saveUninitialized: false,
-        store: new MYSQLStore({}, db) //Use mysql as the session store
+        store: sessionStore, //use mysql as session store
     })
 );
 
-//routes
-app.get('/auth'), (req, res) => {
+
+// Serve static files 
+app.use(express.static(path.join(__dirname)));
+
+
+//fix route declaration
+app.get('/auth', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-};
+});
 
 //Auth routes
-app.use('/auth', authRoutes)
+app.use('/auth', authRoutes);
 
 //Start the server
-const port = process.env.port || 3306;
+const port = process.env.PORT || 3000;
 app.listen(port,() => {
     console.log(`Server is running on http://localhost:${port}`);
 });
